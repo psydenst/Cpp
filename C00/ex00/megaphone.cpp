6382605c9a8a85703a90d76168e2499435207eeb
psydenst <psydenst.dev@gmail.com>
2023-04-17 15:55:38 -0300

    ex00

--- C00/ex00/megaphone.cpp
+++ C00/ex00/megaphone.cpp
@@ -1,10 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psydenst <psydenst@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 15:44:07 by psydenst          #+#    #+#             */
+/*   Updated: 2023/04/17 15:44:09 by psydenst         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
-#include <string>
 
 int main(int argc, char **argv)
 {
 	int i;
+	int j;
+	
 
+	j = 0;
 	i = 1;
 	if (argc == 1)
 	{
@@ -14,7 +28,12 @@ int main(int argc, char **argv)
 	{
 		while (argv[i])
 		{
-			std::cout << std::uppercase << argv[i];
+			while(argv[i][j])
+			{
+				char c = toupper(argv[i][j]);
+				std::cout << c;
+				j++;
+			}
 			i++;
 		}
 		std::cout << std::endl;
