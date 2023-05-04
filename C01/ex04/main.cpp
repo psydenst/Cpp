986e2520c10e75bceba303b8599a5087c0a1c270
psydenst <psydenst.dev@gmail.com>
2023-05-03 22:55:49 -0300

    ex04 incomplete

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: psydenst <psydenst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:18:17 by psydenst          #+#    #+#             */
-/*   Updated: 2023/05/03 20:59:31 by psydenst         ###   ########.fr       */
+/*   Updated: 2023/05/03 22:49:43 by psydenst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,26 +26,55 @@ std::string replace(std::string str1, std::string myString, std::string str2, si
     return (result);
 }
 
-int main(int argc, char **argv) 
+int openFile(char *fileName, std::ifstream &inputFile)
 {
-    
-    std::string str1 = "Hello";
-    std::string str2 = "Hello meu mano";
-    std::ifstream file("file.txt");
+    inputFile.open(fileName);
+    if(!inputFile)
+    {
+        std::cerr << "Error opening the file" << std::endl;
+        return (0);
+    }
+    return (1);
+}
 
-    std::string myString((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    
+int error(int argc, char *fileName, std::ifstream *inputFile)
+{
+    if (argc != 4 || !openFile(fileName, *inputFile))
+    {
+        std::cout << "Usage: ./replace filename str1 str2" << std::endl;
+        return (1);
+    }
+    return (0);
+}
+
+std::string newString(char *str1, char *str2, std::ifstream inputFile)
+{
+    std::string myString((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
+
     std::cout << myString << std::endl;
 
     std::size_t found = myString.find(str1);
 
     if (found != std::string::npos) 
     {
-        std::string ret = replace(str1, myString, str2, found);
+        return (replace(str1, myString, str2, found));
     } 
     else 
     {
         std::cout << "The string '" << str1 << "' was not found in '" << myString << "'." << std::endl;
     }
+    return (NULL);
+}
 
+int main(int argc, char **argv) 
+{
+    std::ifstream inputFile;
+    std::ofstream outputFile;
+    std::string ret;
+
+    if (error(argc, argv[1], &inputFile))
+        return (0);
+    ret = newString(argv[2], argv[3], inputFile);
+  //  writeFile(argv, &outputFile, ret);
+    return (0);
 }
--- C01/ex04/replace.hpp
+++ C01/ex04/replace.hpp
@@ -6,7 +6,7 @@
 /*   By: psydenst <psydenst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 18:32:41 by psydenst          #+#    #+#             */
-/*   Updated: 2023/05/03 20:45:34 by psydenst         ###   ########.fr       */
+/*   Updated: 2023/05/03 22:48:56 by psydenst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,11 @@
 # include <stdlib.h>
 # include <string>
 # include <cstring>
+#include <sstream>
+
     // std::string replace_str()
     // std:string read_str();
     std::string replace(std::string str1, std::string myString, std::string str2, size_t found);
+    std::string newString(char *str1, char *str2, std::ifstream &inputFile);
 
 #endif
