beb300c6f505cc1ad823c950d3fa6b76f4eb32bd
psydenst <psydenst.dev@gmail.com>
2023-05-04 20:47:03 -0300

    ex done

--- C01/ex04/main.cpp
+++ C01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: psydenst <psydenst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 17:18:17 by psydenst          #+#    #+#             */
-/*   Updated: 2023/05/03 22:49:43 by psydenst         ###   ########.fr       */
+/*   Updated: 2023/05/04 20:42:47 by psydenst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ std::string replace(std::string str1, std::string myString, std::string str2, si
     std::stringstream ss;
     ss << cpy << str2 << rest;
     std::string result = ss.str();
-    std::cout << result;
     return (result);
 }
 
@@ -37,8 +36,14 @@ int openFile(char *fileName, std::ifstream &inputFile)
     return (1);
 }
 
-int error(int argc, char *fileName, std::ifstream *inputFile)
+int error(int argc, char *fileName, std::ifstream *inputFile, std::string str1)
 {
+	if (str1 == "")
+	{
+		std::cerr << "Error: str1 cannot be empty" << std::endl;
+	 	std::cout << "Usage: ./replace filename str1 str2" << std::endl;
+		return (1);
+	}
     if (argc != 4 || !openFile(fileName, *inputFile))
     {
         std::cout << "Usage: ./replace filename str1 str2" << std::endl;
@@ -47,24 +52,34 @@ int error(int argc, char *fileName, std::ifstream *inputFile)
     return (0);
 }
 
-std::string newString(char *str1, char *str2, std::ifstream inputFile)
+std::string newString(char *str1, char *str2, std::ifstream *inputFile)
 {
-    std::string myString((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
-
-    std::cout << myString << std::endl;
-
+	std::string ret; 
+    std::string myString((std::istreambuf_iterator<char>(*inputFile)), std::istreambuf_iterator<char>());
     std::size_t found = myString.find(str1);
 
-    if (found != std::string::npos) 
+    if (found == std::string::npos) 
     {
-        return (replace(str1, myString, str2, found));
+    	std::cout << "The string '" << str1 << "' was not found in '" << myString << "'." << std::endl;
+		return ("");
     } 
-    else 
-    {
-        std::cout << "The string '" << str1 << "' was not found in '" << myString << "'." << std::endl;
-    }
-    return (NULL);
+	else
+	{
+		while ((found = myString.find(str1)) != std::string::npos)
+ 			myString = replace(str1, myString, str2, found);
+	}
+	return (myString);
 }
+
+void writeFile (char **argv, std::ofstream *outputFile, std::string ret)
+{
+	std::stringstream ss;
+	ss << argv[1] << ".replace";
+	std::string fileName = ss.str();
+	(*outputFile).open(fileName);
+	*outputFile << ret;
+}
+
 
 int main(int argc, char **argv) 
 {
@@ -72,9 +87,12 @@ int main(int argc, char **argv)
     std::ofstream outputFile;
     std::string ret;
 
-    if (error(argc, argv[1], &inputFile))
+    if (error(argc, argv[1], &inputFile, argv[2]))
         return (0);
-    ret = newString(argv[2], argv[3], inputFile);
-  //  writeFile(argv, &outputFile, ret);
-    return (0);
+    if ((ret = newString(argv[2], argv[3], &inputFile)) != "")
+		writeFile(argv, &outputFile, ret);
+    inputFile.close();
+	outputFile.close();
+	return (0);
 }
+
--- C01/ex04/replace.hpp
+++ C01/ex04/replace.hpp
@@ -6,7 +6,7 @@
 /*   By: psydenst <psydenst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 18:32:41 by psydenst          #+#    #+#             */
-/*   Updated: 2023/05/03 22:48:56 by psydenst         ###   ########.fr       */
+/*   Updated: 2023/05/04 20:44:46 by psydenst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,11 @@
 # include <cstring>
 #include <sstream>
 
-    // std::string replace_str()
-    // std:string read_str();
-    std::string replace(std::string str1, std::string myString, std::string str2, size_t found);
-    std::string newString(char *str1, char *str2, std::ifstream &inputFile);
+// MAIN.CPP
+std::string	replace(std::string str1, std::string myString, std::string str2, size_t found);
+int			openFile(char *fileName, std::ifstream &inputFile);
+int 		error(int argc, char *fileName, std::ifstream *inputFile, std::string str1);
+std::string	newString(char *str1, char *str2, std::ifstream *inputFile);
+void		writeFile (char **argv, std::ofstream *outputFile, std::string ret);
 
 #endif
