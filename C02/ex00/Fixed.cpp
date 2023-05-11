14fa9bcf1fed1fbf785996629b61d20fad997ee5
psydenst <psydenst.dev@gmail.com>
2023-05-11 17:17:17 -0300

    ex00

--- C02/ex00/Fixed.cpp
+++ C02/ex00/Fixed.cpp
@@ -6,22 +6,21 @@
 /*   By: psydenst <psydenst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 19:15:45 by psydenst          #+#    #+#             */
-/*   Updated: 2023/05/09 22:50:01 by psydenst         ###   ########.fr       */
+/*   Updated: 2023/05/11 17:15:54 by psydenst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-static int const Fixed::_fraction_bits = 8;
-
-Fixed::Fixed()
+Fixed::Fixed() : _rawBits(0)
 {
     std::cout << "Default constructor called" << std::endl;
-    this->_rawBits = 0;
+   // this->_rawBits = 0;
 }
 
 Fixed::Fixed(Fixed const &instance)
 {
+    // this->_fraction_bits = 0;
     std::cout << "Copy constructor called" << std::endl;
     *this = instance; // would this->*instance work?
 }
@@ -31,18 +30,20 @@ Fixed::~Fixed()
     std::cout << "Destructor called" << std::endl;
 }
 
-int Fixed::getRawBits(void)
+int Fixed::getRawBits(void) const
 {
     std::cout << "getRawBits member function called" << std::endl;
+    return (this->_rawBits);
 }
 
 void    Fixed::setRawBits(int _rawBits)
 {
-    this->_rawBits = _rawBits;
+    this->_rawBits = _rawBits / _fraction_bits;
 }
 
-Fixed &Fixed::operator =(Fixed &const rigth_hand_side)
+Fixed &Fixed::operator =(Fixed const &rigth_hand_side)
 {
     std::cout << "Assignation operator called" << std::endl;
     this->_rawBits = rigth_hand_side.getRawBits();
+    return (*this);
 }
--- C02/ex00/Fixed.hpp
+++ C02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: psydenst <psydenst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 18:45:14 by psydenst          #+#    #+#             */
-/*   Updated: 2023/05/09 22:50:04 by psydenst         ###   ########.fr       */
+/*   Updated: 2023/05/11 17:16:01 by psydenst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@ class Fixed
 {
     private:
     int     _rawBits;
-    static int const _fraction_bits;
-
+    static int const _fraction_bits = 8;
+    
     public:
     Fixed(void);
     Fixed(Fixed const &instance);
     ~Fixed();
-    int getRawBits(void);
-    void setRawBits(int _rawBits);    
+    int getRawBits(void) const;
+    void setRawBits(int _rawBits);
 
-    Fixed &operator=(Fixed &const rigth_hand_side);
+    Fixed &operator=(Fixed const &rigth_hand_side);
 };
 
 #endif
