b53c2835e2a301b5bae9a2de98926797716a66bf
psydenst <psydenst.dev@gmail.com>
2023-08-16 18:29:34 -0300

    CPP06/ex02 done

--- CPP06/ex02/base.hpp
+++ CPP06/ex02/base.hpp
@@ -1,14 +1,27 @@
 #ifndef BASE_HPP
 # define BASE_HPP
 
+#include <iostream>
+#include <stdlib.h>
+
 class Base
 {
     public: 
     virtual ~Base(void);
 };
 
-class A : public Base{};
-class B : public Base{};
-class C : public Base{};
+class A : public Base
+{
+    ~A();
+};
+class B : public Base
+{
+    ~B();
+};
+class C : public Base
+{
+    ~C();
+};
+
 
 #endif
--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -1,5 +1,25 @@
 #include "base.hpp"
 
+Base::~Base()
+{
+    std::cout << "Base destructor called" << std::endl;
+}
+
+A::~A()
+{
+    std::cout << "A destructor called" << std::endl;
+}
+
+B::~B()
+{
+    std::cout << "B destructor called" << std::endl;
+}
+
+C::~C()
+{
+    std::cout << "C destructor called" << std::endl;
+}
+
 Base *generate(void)
 {
     Base *class_pointer;
@@ -36,4 +56,65 @@ void identify(Base *p)
         std::cout << "C" << std::endl;
 
 }
+
+void identify(Base& p)
+{
+    std::cout << "------- identifying class by reference ------" << std::endl; 
+    try
+    {
+        dynamic_cast<A &>(p);
+        std::cout << "A" << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << " Not A" << std::endl;
+    }
+    try
+    {
+        dynamic_cast<B &>(p);
+        std::cout << "B" << std::endl; 
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << " Not B" << std::endl;
+    }
+    try
+    {
+        dynamic_cast<C &>(p);
+        std::cout << "C" << std::endl;
+    }
+    catch(std::exception &e)
+    {
+        std::cout << e.what() << " Not C" << std::endl;
+    }
+}
+
+int main(void)
+{
+    Base *random_class[5];
+
+    int i = -1;
+    while (++i < 5)
+    {
+        random_class[i] = generate();
+    }
+    Base &reference0 = *random_class[0];
+    Base &reference1 = *random_class[1];
+    Base &reference2 = *random_class[2];
+    Base &reference3 = *random_class[3];
+    Base &reference4 = *random_class[4];
+    identify(random_class[0]);
+    identify(reference0);
+    identify(random_class[1]);
+    identify(reference1);
+    identify(random_class[2]);
+    identify(reference2);
+    identify(random_class[3]);
+    identify(reference3);
+    identify(random_class[4]);
+    identify(reference4);
+    while (i--)
+        delete random_class[i];
+}
+
 
