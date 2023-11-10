e61aa589ac47a645fa8ce46aaa9d2a8ab24eac00
Pedro luis Sydenstricker alvares <psydenst@c1r5s4.42.rio>
2023-11-10 15:54:51 -0300

    str treatment

--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -2,13 +2,25 @@
 
 int main(int argc, char **argv)
 {
+    int i = 1;
+    int j = 0;
     RPN rpn;
     if (argc != 2)
     {
         std::cerr << "Invalid args" << std::endl;
         return -1;
     }
+    while (argv[i][j])
+    {
+        if (!isdigit(argv[i][j]) && argv[i][j] != ' ' && argv[i][j] != '\t'
+            && argv[i][j] != '*' && argv[i][j] != '/' && argv[i][j] != '+' && argv[i][j] != '-')
+        {
+            std::cerr << "Invalid args" << std::endl;
+            return (-1);
+        }
+        j++;
+    }
     std::string str = argv[1];
     rpn.setCalculus(str);
-   // std::cout << rpn.getResult() << std::endl;
+    // std::cout << rpn.getResult() << std::endl;
 }
